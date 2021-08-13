a20351bf092f9271694705586bb33be8a5701dae
Joy Debnath <joy91740@gmail.com>
2021-08-13 21:29:50 +0600

    add about page, service page, pricing page and dynamic all pages

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('contact/', views.contact, name="contact"),
+    path('about/', views.about, name="about"),
+    path('service/', views.service, name="service"),
+    path('pricing/', views.pricing, name="pricing"),
 ]
--- website/views.py
+++ website/views.py
@@ -21,3 +21,14 @@ def contact(request):
         return render(request, 'contact.html', {'message_name': message_name})
     else:
         return render(request, 'contact.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def service(request):
+    return render(request, 'service.html', {})
+
+
+def pricing(request):
+    return render(request, 'pricing.html', {})
